4ca145956460c7ee8f1bf12ce4d9ddf1874367b2
raulsuriajr <raul.halcyondigital@gmail.com>
2021-09-13 16:30:06 +0800

    setup.sh adjusted

--- setup.sh
+++ setup.sh
@@ -6,9 +6,10 @@ if [ ! -f ".gitignore" ]; then
 elif [ ! -f "sites/default/files/private" ]; then
   mkdir -p sites/default/files/private && chmod -R a+w sites/default/files/private
   chmod -R a+w sites/default/files
-elif [ ! -f ]; then
-  cp sites/default/default.settings.php sites/default/settings.php && chmod -R a+w sites/default/settings.php
-  echo "sites/default/settings.php has been created and granted write access"
+elif [ ! -f "sites/default/settings.php" ]; then
+  SETTINGS_PHP="sites/default/settings.php"
+  cp sites/default/default.settings.php sites/default/settings.php && chmod -R a+w $SETTINGS_PHP
+  echo "${SETTINGS_PHP} has been created and granted write access"
 elif [ ! -f "sites/default/services.yml" ]; then
   cp sites/default/default.services.yml sites/default/services.yml
 else
