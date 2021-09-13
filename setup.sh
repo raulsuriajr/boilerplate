3b259075f92478a9001338ee9707fad4f02962fd
raulsuriajr <raul.halcyondigital@gmail.com>
2021-09-13 16:28:14 +0800

    adjusted setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
-
+mkdir tmp && chmod -R a+w tmp
 composer -V && composer install
-
-#FILES=( ".gitignore" "sites/default/files/private" "sites/default/settings.php" "sites/default/services.yml")
-
 if [ ! -f ".gitignore" ]; then
   cp example.gitignore .gitignore
-fi
-
-mkdir tmp && chmod -R a+w tmp
-
-if [ ! -f "sites/default/files/private" ]; then
+elif [ ! -f "sites/default/files/private" ]; then
   mkdir -p sites/default/files/private && chmod -R a+w sites/default/files/private
   chmod -R a+w sites/default/files
-fi
-
-SETTINGS_PHP="sites/default/settings.php"
-if [ ! -f $SETTINGS_PHP ]; then
+elif [ ! -f ]; then
   cp sites/default/default.settings.php sites/default/settings.php && chmod -R a+w sites/default/settings.php
-  echo "${SETTINGS_PHP} has been created and granted write access"
-fi
-
-if [ ! -f "sites/default/services.yml" ]; then
+  echo "sites/default/settings.php has been created and granted write access"
+elif [ ! -f "sites/default/services.yml" ]; then
   cp sites/default/default.services.yml sites/default/services.yml
+else
+  echo "setup complete!"
 fi
 
