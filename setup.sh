49529e05821761aaaf36b04686894e962c076ddf
raulsuriajr <raul.halcyondigital@gmail.com>
2021-09-13 16:14:14 +0800

    updated setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,24 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $1 == "enmod" ]]; then
-  ./vendor/bin/drush en adminimal_admin_toolbar admin_toolbar_tools backup_migrate editor_advanced_link image_effects pathauto twig_field_value twig_tweak
-else
-  composer install
+composer -V && composer install
 
+#FILES=( ".gitignore" "sites/default/files/private" "sites/default/settings.php" "sites/default/services.yml")
+
+if [ ! -f ".gitignore" ]; then
   cp example.gitignore .gitignore
+fi
 
-  mkdir tmp && chmod -R a+w tmp
+mkdir tmp && chmod -R a+w tmp
 
-  if [ ! -f "sites/default/files/private" ]; then
-    mkdir -p sites/default/files/private && chmod -R a+w sites/default/files/private
-    chmod -R a+w sites/default/files
-  fi
+if [ ! -f "sites/default/files/private" ]; then
+  mkdir -p sites/default/files/private && chmod -R a+w sites/default/files/private
+  chmod -R a+w sites/default/files
+fi
 
-  SETTINGS_PHP="sites/default/settings.php"
-  if [ ! -f $SETTINGS_PHP ]; then
-    cp sites/default/default.settings.php sites/default/settings.php && chmod -R a+w sites/default/settings.php
-    echo "${SETTINGS_PHP} has been created and granted write access"
-  fi
+SETTINGS_PHP="sites/default/settings.php"
+if [ ! -f $SETTINGS_PHP ]; then
+  cp sites/default/default.settings.php sites/default/settings.php && chmod -R a+w sites/default/settings.php
+  echo "${SETTINGS_PHP} has been created and granted write access"
+fi
 
+if [ ! -f "sites/default/services.yml" ]; then
   cp sites/default/default.services.yml sites/default/services.yml
 fi
+
